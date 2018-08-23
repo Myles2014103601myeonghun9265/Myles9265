a76656b2c2644f893bcdaae0c88a66cf8b1d17af
Myles2014103601myeonghun9265 <zzz9996@naver.com>
2018-08-23 21:46:09 +0900

    20180823

--- homework_myles.R
+++ homework_myles.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(readxl)
 ##여기까지는 다 아는 패키지 준비하는 방법!
 ## 본격적으로 시작!
-raw_welfare = read.spss(file = "D://R_homework/Data/Koweps_hpc10_2015_beta1.sav", to.data.frame = T)  #raw_welfare라는 원본을 만들어주고 그 안에 spss데이터를 집어넣어줬음!  to.data.frame = T로 안해주면 데이터프레임으로 읽지를 않는다!
+raw_welfare = read.spss(file = "E://Coumputer_Class/R/Myles/Koweps_hpc10_2015_beta1.sav", to.data.frame = T)  #raw_welfare라는 원본을 만들어주고 그 안에 spss데이터를 집어넣어줬음!  to.data.frame = T로 안해주면 데이터프레임으로 읽지를 않는다!
 welfare = raw_welfare  #복사본 생성!
 ##데이터 검토하는 함수들! 이하 생략하겠음
 head(welfare)
@@ -89,13 +89,14 @@ ageg_income = welfare %>%
   filter(!is.na(income)) %>% 
   group_by(ageg, sex) %>% 
   summarise(mean_income = mean(income))
+
 ageg_income
 ggplot(data = ageg_income, aes(x = ageg, y = mean_income, fill = sex)) + 
-  geom_col() +  #fill = sex 가 성별로 가르는 것!
+  geom_col()+   #fill = sex 가 성별로 가르는 것!
   scale_x_discrete(limits = c("age_20", "age_30", "age_40", "age_50", "age_old")) #내가 원하는 순서대로 바꾸는 방법!.
 ggplot(data = ageg_income, aes(x = ageg, y = mean_income, fill = sex)) + 
   geom_col(position = "dodge") +  #position = "dodge"를 적어주면 분리가 된다!
-  scale_x_discrete(limits = c("age_20", "age_30", "age_40", "age_50", "age_old")) 
+  scale_x_discrete(limits = c("age_20", "age_30", "age_40", "age_50", "age_old"))  # 내가 원하는 순서대로 바꾸는 함수.
 
 #나이 및 성별 월급의 차이를 분석하는 방법!
 sex_age = welfare %>% 
@@ -103,8 +104,176 @@ sex_age = welfare %>%
   group_by(age, sex) %>% 
   summarise(mean_income = mean(income))
 ggplot(data = sex_age, aes(x = age, y = mean_income,  col = sex )) + geom_line() #col = sex를 해줘야 성별로 나뉜다.
+welfare <- welfare %>% 
+  mutate(ageg = ifelse(age < 30, "young", ifelse(age <= 59, "middle", "old")))
+
+ageg_income <- welfare %>%
+  filter(!is.na(income)) %>% 
+  group_by(ageg) %>% 
+  summarise(mean_income = mean(income))
+
+ggplot(data = ageg_income, aes(x = ageg, y = mean_income)) + 
+  geom_col() + 
+  scale_x_discrete(limits = c("middle", "young", "old"))
+
+sex_income <- welfare %>% filter(!is.na(income)) %>% group_by(ageg, sex) %>% summarise(mean_income = mean(income))
+
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) + geom_col(position = "dodge") + scale_x_discrete(limits = c("young", "middle", "old"))
+
+class(welfare$code_job)
+table(welfare$code_job)
+
+list_job = read_excel("Koweps_Codebook.xlsx", col_names = T, sheet = 2)
+head(list_job)
+dim(list_job)
+welfare = left_join(welfare, list_job, id = "code_job")
+head(welfare$job)
+welfare %>% 
+  filter(!is.na(code_job)) %>% 
+  select(code_job, job) %>% 
+  head(10)
 
 
+job_income = welfare %>% 
+  filter(!is.na(job) & !is.na(income)) %>% 
+  group_by(job) %>% 
+  summarise(mean_income = mean(income))
+
+head(job_income)
+
+top10 = job_income %>% 
+  arrange(desc(mean_income)) %>% 
+  head(10)
+
+ggplot(data = top10, aes(x = reorder(job, mean_income), y = mean_income)) + 
+  geom_col() + 
+  coord_flip()
+
+bottom10 = job_income %>% 
+  arrange(mean_income) %>% 
+  head(10)
+bottom10
+
+ggplot(data = bottom10, aes(x = reorder(job, -mean_income),
+                            y = mean_income)) + 
+  geom_col() + 
+  coord_flip() + 
+  ylim(0,850)
+
+job_male = welfare %>% 
+  filter(!is.na(job) & sex == "male") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+job_male  
+ 
+job_female = welfare %>% 
+  filter(!is.na(job) & sex == "female") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+job_female
+ggplot(data = job_male, aes(x = reorder(job, n), y = n)) +
+  geom_col() + 
+  coord_flip()
+ggplot(data = job_female, aes(x = reorder(job, n), y = n)) +
+  geom_col() + 
+  coord_flip()
+
+class(welfare$religion)
+table(welfare$religion)
+welfare$religion = ifelse(welfare$religion == 1, "yes", "no")
+table(welfare$religion)
+qplot(welfare$religion)
+class(welfare$marriage)
+table(welfare$marriage)
+welfare$group_marriage = ifelse(welfare$marriage == 1, "marriage",
+                                ifelse(welfare$marriage == 3, "divorce", NA))
+table(welfare$group_marriage)
+table(is.na(welfare$group_marriage))
+qplot(welfare$group_marriage)
 
 
+religion_marriage = welfare %>% 
+  filter(!is.na(group_marriage)) %>% 
+  group_by(religion, group_marriage) %>% 
+  summarise(n = n()) %>% 
+  mutate(tot_group = sum(n)) %>% 
+  mutate(pct = round(n/tot_group*100,1)) # round함수는 반올림을 하라는 함수!
 
+religion_marriage = welfare %>% 
+  filter(!is.na(group_marriage)) %>% 
+  count(religion, group_mariage) %>% 
+  group_by(religion) %>% 
+  mutate(pct = round(n/sum(n)*100, 1))
+
+divorce = religion_marriage %>% 
+  filter(group_marriage == "divorce") %>% 
+  select(religion, pct)
+
+divorce
+
+ggplot(data = divorce, aes(x = religion, y = pct)) + geom_col()
+
+ageg_marriage = welfare %>% 
+  filter(!is.na(group_marriage)) %>% 
+  group_by(ageg, group_marriage) %>% 
+  summarise(n = n()) %>% 
+  mutate(tot_group = sum(n)) %>% 
+  mutate(pct = round(n/tot_group*100, 1))
+
+ageg_divorce = ageg_marriage %>% 
+  filter(ageg != "young" & group_marriage == "divorce") %>% 
+  select(ageg,pct)
+
+
+ageg_religion_marriage = welfare %>% 
+  filter(!is.na(group_marriage) & ageg != "young") %>% 
+  group_by(ageg, religion, group_marriage) %>% 
+  summarise(n = n()) %>% 
+  mutate(tot_group = sum(n)) %>% 
+  mutate(pct = round(n/tot_group*100, 1))
+
+df_divorce = ageg_religion_marriage %>% 
+  filter(group_marriage == "divorce") %>% 
+  select(ageg, religion, pct)
+
+ggplot(data = df_divorce, aes(x = ageg, y = pct, fill = religion)) + 
+  geom_col(position = "dodge")
+
+
+list_region = data.frame(code_region = c(1:7), 
+                         region = c("서울",
+                                    "수도권(인천/경기)",
+                                    "부산/경남/울산",
+                                    "대구/경북",
+                                    "대전/충남",
+                                    "강원/충북",
+                                    "광주/전남/전북/제주도"))
+list_region
+welfare = left_join(welfare, list_region, id = "code_region")
+
+region_ageg = welfare %>% 
+  group_by(region, ageg) %>% 
+  summarise(n = n()) %>% 
+  mutate(tot_group = sum(n)) %>% 
+  mutate(pct = round(n/tot_group*100, 2))
+
+head(region_ageg)
+
+ggplot(data = region_ageg, aes(x = region, y = pct, fill = ageg)) + 
+  geom_col() + 
+  coord_flip()
+
+list_order_old = region_ageg %>% 
+  filter(ageg == "old") %>% 
+  arrange(pct)
+list_order_old
+order = list_order_old$region
+order
+ggplot(data = region_ageg, aes(x = region, y = pct, fill = ageg)) +
+  geom_col() + 
+  coord_flip() +
+  scale_x_discrete(limits = order)
